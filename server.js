3baed805c19392e04d9fcc1ef6bb64a049960424
FerLuque16 <feerluque77@gmail.com>
2021-08-27 00:22:33 -0300

    Modificacion server.js

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = express();
 
 const path = require('path');
 
-app.use(express.static(__dirname + '/my-first-project'));
+app.use(express.static(__dirname + '/angularapp'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/my-first-project/index.html'));
+    res.sendFile(path.join(__dirname + '/angularapp/index.html'));
 });
 
 console.log('Build succesful!!');
