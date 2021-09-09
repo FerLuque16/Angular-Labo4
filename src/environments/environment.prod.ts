c8a84c5c20777e096773abbfe386b1a85dbef0e5
FerLuque16 <feerluque77@gmail.com>
2021-09-09 04:24:17 -0300

    Cambie archivo environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC7xl3vl7EEQ9TbgPfSbt2WTzLi_bHC6tQ",
+    authDomain: "labo4-fer-luque.firebaseapp.com",
+    databaseURL: "https://labo4-fer-luque-default-rtdb.firebaseio.com",
+    projectId: "labo4-fer-luque",
+    storageBucket: "labo4-fer-luque.appspot.com",
+    messagingSenderId: "573094872538",
+    appId: "1:573094872538:web:46c1aae34805a039306ce3"
+  }
 };
